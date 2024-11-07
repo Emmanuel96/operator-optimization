ef47bae99eb319a62dd9350b8e2c82b2e59e2c3f
Emmanuel96 <emmanuel.audu1@aun.edu.ng>
2024-11-07 13:01:03 +0100

    Initial version of operator_optimisation generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/wailo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.wailo.repository;
+
+import com.wailo.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
